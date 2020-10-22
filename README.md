e21255ed40d287bc6f996398c44c3ac1bdbf3784
Sam Beckett <becketts@byu.edu>
2020-10-21 22:26:12 -0600

    add "fair" algorithm

--- README.md
+++ README.md
@@ -11,4 +11,107 @@ Put your contestants in an array of strings in the `contestants.json` file in th
 
 ## How is the name chosen?
 
+### Default
+
+By default, the Crap Distributor picks a contestant from the list at random.
+
 https://stackoverflow.com/a/38448710
+
+### Sorta Fair
+
+The "Fair" mode uses a different algorithim that I think makes it a bit more fair while still allowing for a certain level of randomness. The code is below:
+
+```javascript
+    const groups = checkedHistory
+    // 1. Get the win history for the selected contestants
+      .filter((x) => checked.find((y) => y === x.name))
+    /*
+        2. Group the selected contestants by how many times they've won
+        e.g.:
+        [
+          { count: 2, members: [ Clagius, Arenar ] },
+          { count: 4, members: [ Oritonde, Lydia ] }
+        ]
+    */
+      .reduce((acc, curr) => {
+        const found = acc.findIndex((x) => x.count === curr.count);
+        if (found === -1) {
+          acc.push({ count: curr.count, members: [curr.name] });
+        } else {
+          acc[found].members.push(curr.name);
+        }
+        return acc;
+      }, []);
+
+    // 3. Sort the groups by how many times they've won (descending)
+    groups.sort((a, b) => b.count - a.count);
+    const choiceArray = [];
+    /*
+        4. Loop through the groups. Add each group member to the final array
+        i number of times, where i is the index of the group +1
+        e.g.:
+        [ Clagius, Arenar, Oritonde, Oritonde, Lydia, Lydia ]
+    */
+    for (let i = 0; i < groups.length; i++) {
+      groups[i].members.forEach((member) => {
+        for (let j = 0; j <= i; j++) {
+          choiceArray.push(member);
+        }
+      });
+    }
+    // 5. Pick a random contestant from the array
+    const choice = choiceArray[choiceArray.length * Math.random() | 0];
+```
+
+The contestants are sorted into groups based on how many times they have won. The groups are then sorted in descending order (most times won to least times won). Then, each member is added to a final array of contestants `i` number of times, where `i` is the index of the array + 1.
+
+#### Example
+
+Contestants
+
+```json
+[
+  { "name": "John", "count": 6 },
+  { "name": "George", "count": 5 },
+  { "name": "Pete", "count": 2 },
+  { "name": "Paul", "count": 5 },
+  { "name": "Ringo", "count": 5 },
+]
+```
+
+First, the contestants are sorted into groups based on how many times they have won in the past:
+
+```json
+[
+  {
+    "count": 6,
+    "members": [ "John" ]
+  },
+  {
+    "count": 5,
+    "members": [ "Ringo", "Paul", "George" ]
+  },
+  {
+    "count": 2,
+    "members": [ "Pete" ]
+  }
+]
+```
+
+Then, each contestant is added to a final array based on the array index of the group that they are in +1:
+
+```json
+[ "John", "Ringo", "Ringo", "Paul", "Paul", "George", "George", "Pete", "Pete", "Pete" ]
+```
+
+The final choice is randomly selected from this group. This allows for randomness (John could still win even though he has won the most in the past) while also allowing for fairness (Pete has the highest chance of winning because he has won the least amount of times in the past).
+
+### Data
+
+I ran each method a thousand times, ten thousand times, and a hundred thousand times, and recorded the result after each time. 
+
+My data showed that the "fair" algorithm had a much closer distribution of times won (difference of 6 between highest and lowest after 100,000 iterations) while the completely random algorithm was farther apart (difference of 190 between highest and lowest after 100,000 iterations). 
+
+Additionally, the "fair" algorithm keeps a close distribution at all times, while the completely random algorithm is a lot more random (as expected).
+
+To see the data, see [method-comparison.pdf](method-comparison.pdf)
--- src/picker.jsx
+++ src/picker.jsx
@@ -1,26 +1,16 @@
+/* eslint-disable no-plusplus */
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import ListItemText from '@material-ui/core/ListItemText';
-import Checkbox from '@material-ui/core/Checkbox';
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import { Typography } from '@material-ui/core';
+import {
+  IconButton, Typography, CardContent, CardActions, Card,
+  Button, Checkbox, ListItemText, ListItemSecondaryAction,
+  ListItem, List, Dialog, DialogActions, DialogContent,
+  DialogTitle,
+} from '@material-ui/core';
+import GithubIcon from '@material-ui/icons/GitHub';
+import SettingsIcon from '@material-ui/icons/Settings';
 import { ScaleLoader } from 'react-spinners';
 import contestants from './contestants.json';
-
-// const contestants = [
-//   'Lorraine',
-//   'Dave',
-//   'Paul',
-//   'John',
-//   'Jim',
-//   'Steve',
-// ];
 
 const useStyles = makeStyles((theme) => ({
   listRoot: {
@@ -67,16 +57,58 @@ const useStyles = makeStyles((theme) => ({
   winnerRoot: {
     marginTop: '1vh',
   },
+  hr: {
+    padding: '0',
+    margin: '0 auto',
+    width: '90%',
+    border: '0.5px solid lightgray',
+  },
+  dialogActions: {
+    marginTop: '16px',
+  },
+  dialogContent: {
+    width: '60vw',
+    height: '20vh',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-evenly',
+  },
+  buttonsContainer: {
+    marginTop: '8px',
+    marginBottom: '8px',
+  },
 }));
 
 export default function Picker() {
+  const initCheckedHistory = () => contestants.reduce((acc, cur) => {
+    acc.push({ name: cur, count: 0 });
+    return acc;
+  }, []);
+
+  const getFairModeFromLocalStorage = () => {
+    const val = localStorage.getItem('sortaFairMode');
+    if (val === null) {
+      localStorage.setItem('sortaFairMode', false);
+      return false;
+    }
+    return (val === 'true');
+  };
+  const getCheckedHistoryFromLocalStorage = () => {
+    const val = localStorage.getItem('checkedHistory');
+    if (val === null) {
+      const ch = initCheckedHistory();
+      localStorage.setItem('checkedHistory', JSON.stringify(ch));
+      return ch;
+    }
+    return JSON.parse(val);
+  };
+
   const classes = useStyles();
+  const [dialogOpen, setDialogOpen] = React.useState(false);
+  const [sortaFairMode, setSortaFairMode] = React.useState(getFairModeFromLocalStorage());
   const [checked, setChecked] = React.useState([...contestants]);
   const [winner, setWinner] = React.useState('');
-  const [checkedHistory, setCheckedHistory] = React.useState(contestants.reduce((acc, cur) => {
-    acc[cur] = 0;
-    return acc;
-  }, {}));
+  const [checkedHistory, setCheckedHistory] = React.useState(getCheckedHistoryFromLocalStorage);
   const [waiting, setWaiting] = React.useState(false);
 
   const handleToggle = (value) => () => {
@@ -92,30 +124,131 @@ export default function Picker() {
     setChecked(newChecked);
   };
 
+  const handleOpenDialog = () => {
+    setDialogOpen(true);
+  };
+
+  const handleCloseDialog = () => {
+    setDialogOpen(false);
+  };
+
+  const handleToggleFairMode = () => {
+    setSortaFairMode(!sortaFairMode);
+    localStorage.setItem('sortaFairMode', (!sortaFairMode).toString());
+  };
+
+  const clearHistory = () => {
+    const ch = initCheckedHistory();
+    localStorage.setItem('checkedHistory', JSON.stringify(ch));
+    setCheckedHistory(ch);
+  };
+
+  const findInHistory = (contestant) => checkedHistory.find((x) => x.name === contestant);
+
+  const reportChoice = (choice) => {
+    setWaiting(true);
+    // setTimeout(() => {
+    findInHistory(choice).count += 1;
+    setCheckedHistory(checkedHistory);
+    localStorage.setItem('checkedHistory', JSON.stringify(checkedHistory));
+    setWaiting(false);
+    setWinner(choice);
+    // }, (Math.random() + 0.5) * 1000);
+  };
+
+  const pickSortaFair = () => {
+    const groups = checkedHistory
+    // 1. Get the win history for the selected contestants
+      .filter((x) => checked.find((y) => y === x.name))
+    /*
+        2. Group the selected contestants by how many times they've won
+        e.g.:
+        [
+          { count: 2, members: [ Clagius, Arenar ] },
+          { count: 4, members: [ Oritonde, Lydia ] }
+        ]
+    */
+      .reduce((acc, curr) => {
+        const found = acc.findIndex((x) => x.count === curr.count);
+        if (found === -1) {
+          acc.push({ count: curr.count, members: [curr.name] });
+        } else {
+          acc[found].members.push(curr.name);
+        }
+        return acc;
+      }, []);
+
+    // 3. Sort the groups by how many times they've won (descending)
+    groups.sort((a, b) => b.count - a.count);
+    const choiceArray = [];
+    /*
+        4. Loop through the groups. Add each group member to the final array
+        i number of times, where i is the index of the group +1
+        e.g.:
+        [ Clagius, Arenar, Oritonde, Oritonde, Lydia, Lydia ]
+    */
+    for (let i = 0; i < groups.length; i++) {
+      groups[i].members.forEach((member) => {
+        for (let j = 0; j <= i; j++) {
+          choiceArray.push(member);
+        }
+      });
+    }
+    // 5. Pick a random contestant from the array
+    const choice = choiceArray[choiceArray.length * Math.random() | 0]; // eslint-disable-line
+    reportChoice(choice);
+  };
+
   const pick = () => {
     const choice = checked[checked.length * Math.random() | 0]; // eslint-disable-line
-    setWaiting(true);
-    setTimeout(() => {
-      checkedHistory[choice] += 1;
-      setCheckedHistory(checkedHistory);
-      setWaiting(false);
-      setWinner(choice);
-    }, (Math.random() + 0.5) * 1000);
+    reportChoice(choice);
+  };
+
+  const handlePick = () => {
+    if (sortaFairMode) {
+      pickSortaFair();
+    } else {
+      pick();
+    }
+  };
+
+  const handleSelectAll = () => {
+    if (checked.length === contestants.length) {
+      setChecked([]);
+    } else {
+      setChecked([...contestants]);
+    }
   };
 
   return (
     <div className={classes.root}>
       <Typography className={classes.title} variant="h4">
-        CrapDistributor v0.1
+        CrapDistributor v0.2
       </Typography>
+      <div className={classes.buttonsContainer}>
+        <IconButton onClick={() => window.open('https://github.com/sbeck14/crap-distributor', '_blank')}><GithubIcon /></IconButton>
+        <IconButton onClick={handleOpenDialog}><SettingsIcon /></IconButton>
+      </div>
       <Card className={classes.card}>
         <CardContent className={classes.cardContent}>
           <List dense className={classes.listRoot}>
+            <ListItem key="select-all">
+              <ListItemText primary="Contestants" primaryTypographyProps={{ variant: 'h5' }} />
+              <ListItemSecondaryAction>
+                <Checkbox
+                  edge="end"
+                  onChange={handleSelectAll}
+                  checked={checked.length === contestants.length}
+                  color="primary"
+                />
+              </ListItemSecondaryAction>
+            </ListItem>
+            <hr className={classes.hr} />
             {contestants.map((value) => {
               const labelId = `checkbox-list-secondary-label-${value}`;
               const displayName = (
                 <div className={classes.nameRoot}>
-                  <span className={classes.winCount}>{checkedHistory[value]}</span>
+                  <span className={classes.winCount}>{findInHistory(value).count}</span>
                   <span>{value}</span>
                 </div>
               );
@@ -136,8 +269,13 @@ export default function Picker() {
           </List>
         </CardContent>
         <CardActions className={classes.cardActions}>
-          <Button disabled={checked.length < 2 || waiting} variant="contained" color="primary" onClick={pick}>
-            Pick One!
+          <Button
+            disabled={checked.length < 2 || waiting}
+            variant="contained"
+            color="primary"
+            onClick={handlePick}
+          >
+            Pick a winner
           </Button>
           <Button disabled={waiting} color="secondary" onClick={() => { setChecked([]); setWinner(''); }}>
             Clear
@@ -157,6 +295,29 @@ export default function Picker() {
           </div>
         ) : null}
       </div>
+      <Dialog open={dialogOpen} onClose={handleCloseDialog} maxWidth="md">
+        <DialogTitle id="form-dialog-title">Settings</DialogTitle>
+        <DialogContent className={classes.dialogContent}>
+          <List>
+            <ListItem key="fairMode" button onClick={handleToggleFairMode}>
+              <ListItemText id="fairMode" primary='"Fair" Mode' primaryTypographyProps={{ variant: 'subtitle1' }} />
+              <ListItemSecondaryAction>
+                <Checkbox
+                  edge="end"
+                  onChange={handleToggleFairMode}
+                  checked={sortaFairMode}
+                />
+              </ListItemSecondaryAction>
+            </ListItem>
+          </List>
+          <Button variant="contained" onClick={clearHistory}>Clear Winner History</Button>
+        </DialogContent>
+        <DialogActions className={classes.dialogActions}>
+          <Button onClick={handleCloseDialog} color="primary">
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
